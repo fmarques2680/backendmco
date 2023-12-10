7394e09cae1891054717f67573e2725a80ae34a2
Felipe <fmarques2680@gmail.com>
2023-12-09 21:53:11 -0300

    configuraçoes

--- app.js
+++ app.js
@@ -60,10 +60,10 @@ router.put('/', async function (req,res) {
 app.use(bodyParser.json());
 app.use(`/api`, router);
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "https://backendmco.vercel.app/api");
-    app.use(cors());
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header("Access-Control-Allow-Origin", "https://backendmco.vercel.app/api");
+    res.header("Access-Control-Allow-Methods", 'GET,POST,PUT,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    app.use(cors());
     next();
   });
 export default app;
